ed2e67c6914b096297aafb19e917347430ae9b42
Md.Shajin Khan <mdsazinur90@yahoo.com>
2026-02-01 20:14:09 +0600

    fix teacher register 500 by removing select:false

--- src/models/Teacher.js
+++ src/models/Teacher.js
@@ -5,7 +5,7 @@ const teacherSchema = new mongoose.Schema(
   {
     name: { type: String, default: "" },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, select: false },
+    password: { type: String, required: true},
     subject: { type: String, default: "" },
     phone: { type: String, default: "" },
     avatar: { type: String, default: "" },
